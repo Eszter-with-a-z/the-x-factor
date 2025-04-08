500912f59a52685aae9720f6c2d0a9c391033e52
Eszter-with-a-z <85640262+Eszter-with-a-z@users.noreply.github.com>
2025-04-08 23:58:13 +0200

    Add my_fucntions and pick_topic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 import requests
+import my_functions
 
 app = Flask(__name__)
 
@@ -13,12 +14,18 @@ def index():
 
 @app.route('/chat', methods=['POST'])
 def chat():
+    topic = my_functions.pick_random_topic()
+    print(topic)
     user_message = request.json.get('message')
     response = requests.post(OLLAMA_API_URL, json={
         'model': 'gemma3:4b',
         'prompt': user_message,
         "stream": False,
-        'system': 'You are talking to a craft entrepreneur. You are curious about how they started their craft.',
+        'system': (f"""
+        You are talking to a craft entrepreneur. You 
+        are curious about how ther unique view on crafting
+        and {topic}.
+        """),
         'context': [],
     })
     return jsonify(response.json())
---
+++ my_functions.py
@@ -0,0 +1,7 @@
+# Generate random question topic
+import random
+
+topics = ["ethics","sustainability", "human-scale", "being personal", "uniqueness", "longevity of the products"]
+
+def pick_random_topic():
+    return random.choice(topics)
